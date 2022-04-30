fe5b700aefb74c2b152c21536cd3f3c9f04a2b53
jpcreativeworks <jp.graphics.011@gmail.com>
2022-04-30 16:01:40 -0700

    email request routes created

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const episodicRoutes = require('./episodicRoutes');
 const inventoryRoutes = require('./inventoryRoutes');
+const emailRoutes = require('./emailRequestRoutes');
 
+router.use('/email', emailRoutes);
 router.use('/users', userRoutes);
 router.use('/episodics', episodicRoutes);
 router.use('/inventories', inventoryRoutes);
