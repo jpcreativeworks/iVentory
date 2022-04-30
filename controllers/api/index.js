958e59d66128c3f04b4049222a65031846d72a30
jpcreativeworks <jp.graphics.011@gmail.com>
2022-04-30 15:30:02 -0700

    updated form finish and index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const episodicRoutes = require('./episodicRoutes');
 const inventoryRoutes = require('./inventoryRoutes');
+const formFinish = require("./formFinishRoute");
 
 router.use('/users', userRoutes);
 router.use('/episodics', episodicRoutes);
 router.use('/inventories', inventoryRoutes);
-
+router.use('/forms', formFinish);
 module.exports = router;
